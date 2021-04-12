87432a48714cc11f663c286b17ff3e44dc265395
psy6190 <psy6190@gmail.com>
2021-04-12 23:07:26 +0900

    서블릿의 lifecycle 및 간단한 doGet, doPost 활용

---
+++ firstWeb/src/examples/LifeCycleServlet.java
@@ -0,0 +1,73 @@
+package examples;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LifeCycleServlet")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public LifeCycleServlet() {
+        System.out.println("LifeCycleServlet 생성");
+        
+    }
+
+
+	public void init(ServletConfig config) throws ServletException {
+        System.out.println("Init 호출");
+	}
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstWeb/LifeCycleServlet'>");
+		out.println("namee : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");
+		out.println("</form>");
+		
+		// header 의 정보를 나타내주기 위함
+		Enumeration<String> headerNames = req.getHeaderNames();
+		while(headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			String headerValue = req.getHeader(headerName);
+			out.println(headerName + ':' + headerValue + "<br>");
+		}
+		
+		String name = req.getParameter("name");
+		String age = req.getParameter("age");
+		out.println("name:" + name + "<br>");
+		out.println("age:" + age + "<br>");
+		
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		out.println("<h1> hello~" + name + "</h1>");
+		out.close();
+	}
+
+
+	
+	
+}
